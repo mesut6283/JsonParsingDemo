ab143ffba7f0c8e082080c88be49021ad8bf302f
mesut6283 <masoud_m_tr@yahoo.com>
2016-03-23 23:15:14 +0200

    JsonReadWrite

--- app/src/main/java/com/masoud/example/jsonparsingdemo/MainActivity.java
+++ app/src/main/java/com/masoud/example/jsonparsingdemo/MainActivity.java
@@ -1,13 +1,86 @@
 package com.masoud.example.jsonparsingdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHit = (Button) findViewById(R.id.btnHit);
+        tvData = (TextView) findViewById(R.id.tvJsonItem);
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JSONTask().execute("https://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
+            }
+        });
+
+
+    }
+
+    public class JSONTask extends AsyncTask<String,String,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            HttpsURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(connection != null)
+                {
+                    connection.disconnect();}
+                try {
+                    if(reader != null){
+                        reader.close();}
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+          return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+        }
     }
 }
